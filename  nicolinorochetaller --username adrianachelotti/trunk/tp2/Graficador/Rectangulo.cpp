5aa4e6476dab15b99c30613d94ef548d45d3f270
dmpstaltari <dmpstaltari@0b808588-0f27-11de-aab3-ff745001d230>
2009-05-13 05:41:07 +0000

    Se agrega a dibujar de las figuras la funcionalidad de tomar la textura del escenario en caso de que la figura no poseea textura (o la misma no se pueda cargar) ni color de fondo.

---  nicolinorochetaller --username adrianachelotti/trunk/tp2/Graficador/Rectangulo.cpp
+++  nicolinorochetaller --username adrianachelotti/trunk/tp2/Graficador/Rectangulo.cpp
@@ -104,6 +104,39 @@ int Rectangulo::dibujar()
 			contextoError+= this->getId();
 	
 			escenario->imprimirError(contextoError,escenario->getArchivoErrores(),mensajeError);
+		
+				//Se intenta cargar la textura del escenario
+			if((!Escenario::getTexturaFigura().empty()) && (!this->esColorPropio()))
+			{
+				text = escenario->getTextura(Escenario::getTexturaFigura());
+				
+				if(text==NULL)
+				{
+					string contextoError = MSG_CTX_FIGURA;
+					contextoError+= this->getId();
+					
+					string mensajeError = GRAF_WARN1;
+					mensajeError+= Escenario::getTexturaFigura();
+					mensajeError+= " - ";
+
+					escenario->imprimirError(contextoError,escenario->getArchivoErrores(),mensajeError);
+				}
+				else
+				{
+					imagen = SDL_LoadBMP(text->getPath().c_str());
+					if(imagen==NULL)
+					{
+						string contextoError = MSG_CTX_FIGURA;
+						contextoError+= this->getId();
+							
+						string mensajeError = GRAF_WARN2;
+						mensajeError+= Escenario::getTexturaFigura();
+						mensajeError+= " - ";
+
+						escenario->imprimirError(contextoError,escenario->getArchivoErrores(),mensajeError);
+					}
+				}
+			}
 		}
 	}
 
---  nicolinorochetaller --username adrianachelotti/trunk/tp2/Graficador/Triangulo.cpp
+++  nicolinorochetaller --username adrianachelotti/trunk/tp2/Graficador/Triangulo.cpp
@@ -129,6 +129,39 @@ int Triangulo::dibujar()
 			contextoError+= this->getId();
 	
 			escenario->imprimirError(contextoError,escenario->getArchivoErrores(),mensajeError);
+		
+				//Se intenta cargar la textura del escenario
+			if((!Escenario::getTexturaFigura().empty()) && (!this->esColorPropio()))
+			{
+				text = escenario->getTextura(Escenario::getTexturaFigura());
+				
+				if(text==NULL)
+				{
+					string contextoError = MSG_CTX_FIGURA;
+					contextoError+= this->getId();
+					
+					string mensajeError = GRAF_WARN1;
+					mensajeError+= Escenario::getTexturaFigura();
+					mensajeError+= " - ";
+
+					escenario->imprimirError(contextoError,escenario->getArchivoErrores(),mensajeError);
+				}
+				else
+				{
+					imagen = SDL_LoadBMP(text->getPath().c_str());
+					if(imagen==NULL)
+					{
+						string contextoError = MSG_CTX_FIGURA;
+						contextoError+= this->getId();
+							
+						string mensajeError = GRAF_WARN2;
+						mensajeError+= Escenario::getTexturaFigura();
+						mensajeError+= " - ";
+
+						escenario->imprimirError(contextoError,escenario->getArchivoErrores(),mensajeError);
+					}
+				}
+			}
 		}
 	}
 
@@ -136,31 +169,29 @@ int Triangulo::dibujar()
 	{
 		if((this->getColorFondo()!=COLOR_VACIO))
 		{
-			
 			graficador->rellenarTriangulo(Escenario::screen,&(this->vertices[0]),&(this->vertices[1]),&(this->vertices[2]),this->getColorFondo());
 			
 			if(this->getColorLinea()!=COLOR_VACIO)
 			{
-			graficador->dibujarTriangulo(Escenario::screen,this->vertices[0],this->vertices[1],this->vertices[2],this->getColorLinea());
+				graficador->dibujarTriangulo(Escenario::screen,this->vertices[0],this->vertices[1],this->vertices[2],this->getColorLinea());
 			}
 		}
 		else
 		{
-			//TODO
 			graficador->dibujarTriangulo(Escenario::screen,this->vertices[0],this->vertices[1],this->vertices[2],this->getColorLinea());
 		
-
 		}
 	
 		if(contieneTextura) return RES_ERROR_CARGANDO_TEXTURA;
 		return RES_OK;
 		
-	}else
+	}
+	else
 	{
-			//dibujar Textura
+		//dibujar Textura
 	
 		SDL_Surface* imagenResized  = graficador->getImageResized(text,this->getAncho(), this->getAlto());
-		//SDL_SaveBMP(imagen, "bh.bmp");
+	
 
 		graficador->rellenarTrianguloConTextura(Escenario::screen,imagenResized ,&this->vertices[0],&this->vertices[1],&this->vertices[2]);
 
