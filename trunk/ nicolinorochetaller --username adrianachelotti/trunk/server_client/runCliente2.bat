25df07ffe284e884fb45fc0dfb03ac33edfbb2a2
adrianachelotti@gmail.com <adrianachelotti@gmail.com@0b808588-0f27-11de-aab3-ff745001d230>
2009-06-20 00:39:51 +0000

    Se corrige la forma de recibir en el server

--- trunk/ nicolinorochetaller --username adrianachelotti/trunk/server_client/runCliente2.bat
+++ trunk/ nicolinorochetaller --username adrianachelotti/trunk/server_client/runCliente2.bat
@@ -1,5 +1,5 @@
 set Y = %CD%
 cd %Y%
-del .\clientedeprueba\imagenesTransferidas\*.bmp
-del .\clientedeprueba2\imagenesTransferidas\*.bmp
-call .\clientedeprueba2\testClient\Debug\Cliente04.exe localhost 5001
+rem del .\clientedeprueba\imagenesTransferidas\*.bmp
+rem del .\clientedeprueba2\imagenesTransferidas\*.bmp
+call .\clientedeprueba2_1\testClient\Debug\Cliente04.exe localhost 5001
