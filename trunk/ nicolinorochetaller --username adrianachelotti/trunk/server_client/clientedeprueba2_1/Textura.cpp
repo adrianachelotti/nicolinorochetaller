23cd25226a2cc91e6c69bd3f469533d8e1ab4f2a
adrianachelotti@gmail.com <adrianachelotti@gmail.com@0b808588-0f27-11de-aab3-ff745001d230>
2009-06-20 01:00:14 +0000

    archivos faltantes del cliente 2

---
+++ trunk/ nicolinorochetaller --username adrianachelotti/trunk/server_client/clientedeprueba2_1/Textura.cpp
@@ -0,0 +1,59 @@
+// Textura.cpp: implementation of the Textura class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "Textura.h"
+
+Textura::Textura()
+{
+
+}
+
+Textura::~Textura()
+{
+
+}
+
+
+Textura::Textura(string id,string path)
+{
+	this->id = id;
+	this->path = path;
+	//TODO
+	this->imagen =  NULL;
+	
+	
+}
+
+void Textura::setId(string id)
+{
+	this->id = id;
+}
+
+string Textura::getId()
+{
+	return this->id;
+}
+
+void Textura::setPath(string path)
+{
+	this->path = path;
+}
+
+string Textura::getPath()
+{
+	return this->path;
+}
+SDL_Surface* Textura::getImagen()
+{
+	if(this->imagen==NULL)
+	{
+		this->imagen =  SDL_LoadBMP(path.c_str());
+	}
+	
+	return this->imagen;
+}
+void Textura::setImagen(SDL_Surface* imagen)
+{
+	this->imagen = imagen;
+}
