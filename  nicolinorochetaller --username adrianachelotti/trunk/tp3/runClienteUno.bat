ab6b08f5bfc1d9660f87977f6f1fc036c8ad9dcc
dmpstaltari@gmail.com <dmpstaltari@gmail.com@0b808588-0f27-11de-aab3-ff745001d230>
2009-08-04 04:08:09 +0000

    creacion escenarios

---  nicolinorochetaller --username adrianachelotti/trunk/tp3/runClienteUno.bat
+++  nicolinorochetaller --username adrianachelotti/trunk/tp3/runClienteUno.bat
@@ -3,4 +3,5 @@ cd %Y%
 del .\ClienteUno\Cliente\Debug\archivos\*.bmp
 del .\ClienteUno\Cliente\Debug\errores\*.bmp
 cd .\ClienteUno\Cliente\Debug
+rem call .\Cliente04.exe localhost 5000
 call .\Cliente04.exe localhost 5000
